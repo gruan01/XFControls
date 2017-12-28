85e77237b27bde3ca6174f38cc26385dd816d4d1
Maria Torres <maria.torres@sonorasoft.com>
2017-12-28 11:43:16 -0700

    added htmllabel

--- XFControls/Src/AsNum.XFControls.iOS/AsNumAssemblyHelper.cs
+++ XFControls/Src/AsNum.XFControls.iOS/AsNumAssemblyHelper.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Plugin.HtmlLabel.iOS;
+using System;
 namespace AsNum.XFControls.iOS
 {
 	public static class AsNumAssemblyHelper
 	{
 		public static void HoldAssembly()
 		{
-			// Do nothing, just hold assembly,
-			// prevent linker remove this assembly.
-		}
+            // Do nothing, just hold assembly,
+            // prevent linker remove this assembly.
+            HtmlLabelRenderer.Initialize();
+        }
 	}
 }
